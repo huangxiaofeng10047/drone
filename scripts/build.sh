2e2fcfd3bb527a2dafcd8348c45f8a0997fbdb6b
hxf <570195314@qq.com>
2021-08-25 08:37:31 +0800

    fix(build): 添加go代理

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,6 +3,7 @@
 echo "building docker images for ${GOOS}/${GOARCH} ..."
 
 REPO="github.com/drone/drone"
-
+export GO111MODULE=on 
+ export GOPROXY=https://goproxy.cn
 # compile the server using the cgo
 go build -ldflags "-extldflags \"-static\"" -o release/linux/${GOARCH}/drone-server ${REPO}/cmd/drone-server
